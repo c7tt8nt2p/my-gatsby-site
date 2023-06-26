e9008a22161b20c313f5840555c12c83ec4e7b8a
Chantapat Tancharoen <chantapat.t@kbtg.tech>
2023-06-26 14:10:31 +0700

    test

--- src/pages/form.js
+++ src/pages/form.js
@@ -2,20 +2,20 @@ import React, {useState} from 'react';
 
 const Register = () => {
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        const myForm = e.target;
-        const formData = new FormData(myForm);
-
-        fetch("/", {
-            method: "POST",
-            headers: {"Content-Type": "application/x-www-form-urlencoded"},
-            body: new URLSearchParams(formData).toString(),
-        })
-            .then(() => console.log("Form successfully submitted"))
-            .catch((error) => alert(error));
-    };
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+    //
+    //     const myForm = e.target;
+    //     const formData = new FormData(myForm);
+    //
+    //     fetch("/", {
+    //         method: "POST",
+    //         headers: {"Content-Type": "application/x-www-form-urlencoded"},
+    //         body: new URLSearchParams(formData).toString(),
+    //     })
+    //         .then(() => console.log("Form successfully submitted"))
+    //         .catch((error) => alert(error));
+    // };
 
     return (
         <form name="contact" method="POST" data-netlify="true">
